6277f2e1a42979970e60f8925affa56adbf3d8f9
revvann <rghofur053@gmail.com>
2022-05-15 00:14:32 +0700

    change pubspect raw

--- bin/constant.dart
+++ bin/constant.dart
@@ -1,4 +1,4 @@
 // ignore_for_file: constant_identifier_names
 
 const String GITHUB_RAW_URL = 'https://api.github.com/repos/rev1853/scarab/contents/raw';
-const String GITHUB_TOKEN = 'rev1853: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';
+const String GITHUB_TOKEN = 'token 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';
