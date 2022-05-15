0b75c1bd28b72824ab4a1417b0474445027ef1e3
revvann <rghofur053@gmail.com>
2022-05-15 10:30:36 +0700

    change token

--- bin/constant.dart
+++ bin/constant.dart
@@ -1,4 +1,4 @@
 // ignore_for_file: constant_identifier_names
 
 const String GITHUB_RAW_URL = 'https://api.github.com/repos/rev1853/scarab/contents/raw';
-const String GITHUB_TOKEN = 'token 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';
+const String GITHUB_TOKEN = 'token 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';
