89ae45ba4af0c45b262ed4850dceb6e5e5f98e36
revvann <rghofur053@gmail.com>
2022-05-15 10:19:38 +0700

    change  fuckin little typo

--- bin/constant.dart
+++ bin/constant.dart
@@ -1,4 +1,4 @@
 // ignore_for_file: constant_identifier_names
 
 const String GITHUB_RAW_URL = 'https://api.github.com/repos/rev1853/scarab/contents/raw';
-const String GITHUB_TOKEN = 'token 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';
+const String GITHUB_TOKEN = 'token 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';
